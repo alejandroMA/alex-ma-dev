f5ecdc819c0abc436905cfd90e56ccaa38a30a34
Alejandro Madariaga Angeles <amndell@gmail.com>
2020-01-17 17:03:59 -0600

    contact links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome'
+import {
+    faTwitter,
+    faLinkedin,
+    faGithub
+} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 // import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
@@ -22,7 +29,7 @@ function IndexPage() {
 
     return (
         <Layout>
-            <SEO title="Home" />
+            <SEO title="Hi" />
 
             <header className="header">
                 <div className="spacer"></div>
@@ -34,18 +41,42 @@ function IndexPage() {
                 <h3>Software Engineer in Querétaro, México</h3>
             </header>
 
-            <div>
+            <div className="links">
                 <ul>
                     <li>
                         <a href={data.resume.publicURL}>Resume</a>
                     </li>
                     <li>
-                        <a href={data.portfolio.publicURL}>Potfolio</a>
-                    </li>
-                    <li>
-                        <a href=""></a>
+                        <a href={data.portfolio.publicURL}>Portfolio</a>
                     </li>
                 </ul>
+            </div>
+
+            <div className="icons">
+                <a
+                    href="https://github.com/alejandroMA"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FAIcon icon={faGithub} />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/alejandro-madariaga-angeles/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FAIcon icon={faLinkedin} />
+                </a>
+                <a
+                    href="https://twitter.com/alejandromadari"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FAIcon icon={faTwitter} />
+                </a>
+                <a
+                    href="mailto:amndell@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FAIcon icon={faEnvelope} />
+                </a>
             </div>
         </Layout>
     )
